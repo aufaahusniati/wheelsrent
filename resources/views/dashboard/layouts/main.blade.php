7412341e3cd9ad52d65fa5e8661af5daacdead04
indrianti10 <indriantiii10@gmail.com>
2023-12-02 14:16:43 +0700

    menyelesaikan Sidebar

---
+++ resources/views/dashboard/layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>WheelsRent | Dashboard</title>
+    <link rel="stylesheet" href="dist/output.css">
+
+    {{-- Tailwind --}}
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
+</head>
+<body>
+
+{{-- Sidebar --}}
+@include('dashboard.layouts.sidebar')
+{{-- End Sidebar --}}
+   
+  
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
+</body>
+</html>
