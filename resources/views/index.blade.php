c12ea6e0a6e3a40443fac7f165cc3c1658ecb016
aufaahusniati <aufaahsnt21@gmail.com>
2023-12-09 14:11:29 +0700

    Menyelasaikan tampilan tentang kami

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,25 +8,25 @@
             <div id="controls-carousel" class="relative w-full z-0" data-carousel="static">
                 <!-- Carousel wrapper -->
                 <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-                    <!-- Item 1 -->
+                    <!-- Slide 1 -->
                     <div class="flex flex-wrap duration-700 ease-in-out bg-white" data-carousel-item="active" >
-                        <img src="/img/slide1.jpg" class="absolute block w-full bg-opacity-10 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" style="background-color: rgba(0, 0, 0, 0.6)" alt="...">
+                        <img src="/img/slide1.jpg" class="absolute block bg-opacity-10 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                         <div class="absolute w-full self-center ml-[65px] lg:w-1/2">
-                            <h1 class="text-base font-bold text-white lg:text-4xl">Dapatkan pengalaman terbaik Anda dengan WheelsRent</h1>
+                            <h1 class="text-xs font-bold text-white lg:text-4xl">Dapatkan pengalaman terbaik Anda dengan WheelsRent</h1>
                             <p class="text-xs mt-3 mb-5 mr-20 text-slate-300 leading-relaxed lg:text-lg">Memberikan transportasi dan solusi terbaik untuk menjawab kebutuhan mobilitas anda sehari-hari</p>
                                 
                             <a href="#" class="text-xs font-semibold text-white bg-primary py-[8px] px-[15px] rounded-full hover:bg-white hover:text-primary transition duration-300 ease-in-out lg:text-base lg:py-2 lg:px-8 ">Reservasi</a>
                         </div>
                     </div>
 
-                    <!-- Item 2 -->
+                    <!-- Slide 2 -->
                     <div class="flex flex-wrap duration-700 ease-in-out bg-white" data-carousel-item >
-                        <img src="/img/slide2.jpg" class="absolute block w-full bg-opacity-10 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" style="background-color: rgba(0, 0, 0, 0.6)" alt="...">
+                        <img src="/img/slide2.jpg" class="absolute block w-full bg-opacity-10 -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                         <div class="absolute w-full self-center ml-[65px] lg:w-1/2">
-                            <h1 class="text-base font-bold text-white lg:text-4xl">Jasa Sewa Mobil Bandung</h1>
-                            <p class="text-xs mt-3 mb-5 mr-[80px] text-slate-300 leading-relaxed lg:text-lg">WheelsRent merupakan perusahaan yang bergerak dibidang jasa rental sesuai dengan kebutuhan pelanggan.</p>
+                            <h1 class="text-xs font-bold text-white lg:text-4xl">Jasa Sewa Mobil Bandung</h1>
+                            <p class="text-xs mt-2 mb-3 mr-[80px] text-slate-300 leading-relaxed lg:text-lg">WheelsRent merupakan perusahaan yang bergerak dibidang jasa rental sesuai dengan kebutuhan pelanggan.</p>
 
-                            <a href="#" class="text-xs font-semibold text-white bg-primary py-[7px] px-[15px] rounded-full hover:bg-white hover:text-primary transition duration-300 ease-in-out lg:text-base lg:py-2 lg:px-8 ">Cek Harga</a>
+                            <a href="#" class="text-xs font-semibold text-white bg-primary py-[5px] px-[15px] rounded-full hover:bg-white hover:text-primary transition duration-300 ease-in-out lg:text-base lg:py-2 lg:px-8">Cek Harga</a>
                         </div>
                     </div>
                 </div>
@@ -53,5 +53,23 @@
     </section>
     <!-- Hero Section End -->
 
+    <!-- Tentang Kami Section Start -->
+    <section id="about" class="pt-36">
+        <div class="container">
+            <div class="gap-2 lg:grid lg:grid-cols-2">
+                <div>
+                    <img class="h-auto max-w-full rounded-lg" src="/img/tentangkami.png" alt="">
+                </div>
+                <div>
+                    <h1 class="text-xl uppercase font-extrabold text-primary lg:text-4xl">Tentang WheelsRent</h1>
+                    <p class="text-sm mt-3 mb-5 text-dark lg:text-lg">Wheelsrent memberikan Anda pengalaman terbaik dengan solusi transportasi sewa mobil dan layanan pengemudi profesional. Memulai perjalanannya dengan menyewa lima mobil pada tahun 2023, wheelsrent telah berkembang menjadi salah satu perusahaan transportasi terbesar di Indonesia di bawah PT Ode to Code. Saat ini Wheelsrent memiliki 30 ribu kendaraan yang siap melayani Anda, siap menunjang kebutuhan mobilitas Anda. Dengan menjunjung tinggi profesionalitas, Wheelsrent telah dipercaya oleh ribuan pelanggan bisnis maupun individu.</p>
+
+                    <a href="#" class="text-xs font-semibold py-[7px] px-[15px] text-white bg-primary rounded-full border-2 border-primary hover:bg-white hover:text-primary focus:z-10 focus:ring-2 focus:ring-primary focus:text-primary dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white lg:text-base lg:py-2 lg:px-8">Selengkapnya</a>
+                </div> 
+            </div>
+        </div>
+    </section>
+    <!-- Tentang Kami Section End -->
+    
 @endsection
 
